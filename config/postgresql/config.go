32f42f0fd4384b319b72d85697588c70ac861ca4
Gabriela Limberea <gabriela.limberea@ionos.com>
2024-09-10 11:55:24 +0300

    make reviewable

--- config/postgresql/config.go
+++ config/postgresql/config.go
@@ -4,6 +4,7 @@ import (
 	"github.com/crossplane/upjet/pkg/config"
 )
 
+// Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator(
 		"ionoscloud_pg_cluster", func(r *config.Resource) {
